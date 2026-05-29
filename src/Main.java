b35e7303978534278ce4321db7e1fb6ada9d4dc7
JhoannPV <91164583+JhoannPV@users.noreply.github.com>
2026-05-29 14:09:17 -0500

    nuevo main()

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import app.App;
 
-public class Main {
-    public static void main(String[] args) {
-        App app = new App();
-        app.start();
-    }
+void main() {
+    App app = new App();
+    app.start();
 }
